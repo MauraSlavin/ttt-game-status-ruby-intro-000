b375529b31829df03f95217626c225e3db4e5c5d
Maura Slavin <mauramslavin@gmail.com>
2018-09-11 03:20:39 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -8,11 +8,11 @@ end
 def won?(board)
 
   # display board
-  puts " #{board[0]} | #{board[1]} | #{board[2]}"
-  puts "---|---|---"
-  puts " #{board[3]} | #{board[4]} | #{board[5]}"
-  puts "---|---|---"
-  puts " #{board[6]} | #{board[7]} | #{board[8]}"
+    # puts " #{board[0]} | #{board[1]} | #{board[2]}"
+    # puts "---|---|---"
+    # puts " #{board[3]} | #{board[4]} | #{board[5]}"
+    # puts "---|---|---"
+    # puts " #{board[6]} | #{board[7]} | #{board[8]}"
 
   winner = false
   WIN_COMBINATIONS.each do |win|
