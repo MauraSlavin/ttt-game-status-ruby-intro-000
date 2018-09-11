a23f83d7f46ce36055f4bf46280cff68d09447b4
Maura Slavin <mauramslavin@gmail.com>
2018-09-11 02:03:26 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,6 +3,24 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
+
+
+def won?(board)
+  winner = false
+  WIN_COMBINATIONS.each do |win|
+    winner_case = win.all? do |cell|
+      cell = "X"
+    end  # winner_case .all?
+    winner = winner | winner_case
+    winner_case = win.all? do |cell|
+      cell = "O"
+    end
+    winner = winner | winner_case
+  end
+  winner
+end
+        
+    
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
   [0, 1, 2],
@@ -14,3 +32,4 @@ WIN_COMBINATIONS = [
   [0, 4, 8],
   [2, 4, 6]
 ]
+
